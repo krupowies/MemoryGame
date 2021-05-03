60b954e5b9d2b845ac9935eeff8d88eb4c8e8e05
Wojciech Krupowies <krupowieswojciech@gmail.com>
2021-05-03 20:37:38 +0200

    Initial Commit

---
+++ MemoryGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MemoryGame
+//
+//  Created by Wojtek Krupowies on 03/05/2021.
+//  Copyright © 2021 Wojtek Krupowies. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
