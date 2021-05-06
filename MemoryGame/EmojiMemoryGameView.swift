4e721834814a9745754ea90515df2dea2e5589db
Wojciech Krupowies <krupowieswojciech@gmail.com>
2021-05-06 20:20:34 +0200

    finished lecture 3

--- MemoryGame/EmojiMemoryGameView.swift
+++ MemoryGame/EmojiMemoryGameView.swift
@@ -13,7 +13,7 @@ struct EmojiMemoryGameView: View {
     
     var body: some View {
         HStack{
-            ForEach(viewModel.cards.shuffled()){ card in
+            ForEach(viewModel.cards){ card in
                 CardView(card: card).onTapGesture {
                     self.viewModel.choose(card: card)
                 }
@@ -21,26 +21,40 @@ struct EmojiMemoryGameView: View {
         }
         .padding()
         .foregroundColor(Color.orange)
-        .font(viewModel.cards.count == 10 ? Font.caption : Font.largeTitle)
     }
 }
 
 struct CardView: View{
     var card: Game<String>.Card
     
-    var body: some View{
-        
+    var body: some View {
+        GeometryReader { geometry in
+            self.body(for: geometry.size)
+        }
+    }
+    
+    func body(for size: CGSize) -> some View {
         ZStack{
-            if card.isFaceUp {
-                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
-                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
-                Text(card.content)
+            if self.card.isFaceUp {
+                RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
+                RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
+                Text(self.card.content)
             } else {
-                RoundedRectangle(cornerRadius: 10.0).fill()
+                RoundedRectangle(cornerRadius: cornerRadius).fill()
             }
         }
-        .aspectRatio(0.66, contentMode: .fit)
+        .font(Font.system(size: fontSize(for: size)))
     }
+    
+    // MARK: - Drawing Constants
+    
+    let cornerRadius: CGFloat = 10.0
+    let edgeLineWidth: CGFloat = 3
+    
+    func fontSize(for size: CGSize) -> CGFloat{
+        min(size.width, size.height) * 0.75
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
