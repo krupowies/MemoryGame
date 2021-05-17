aa13997b02aa8d4085de8080f4a37d968a00106d
Wojciech Krupowies <krupowieswojciech@gmail.com>
2021-05-17 13:16:02 +0200

    Finished lecture 6

--- MemoryGame/EmojiMemoryGameView.swift
+++ MemoryGame/EmojiMemoryGameView.swift
@@ -15,14 +15,18 @@ struct EmojiMemoryGameView: View {
         VStack{
             Grid (viewModel.cards){ card in
                 CardView(card: card).onTapGesture {
-                    self.viewModel.choose(card: card)
+                    withAnimation(.linear(duration: 1)){
+                     self.viewModel.choose(card: card)
+                    }
                 }
                 .padding(5)
             }
             Spacer()
             HStack{
                 FilledButton(title: "NEW GAME") {
-                    self.viewModel.newGame()
+                    withAnimation(.easeInOut(duration: 1)){
+                     self.viewModel.newGame()
+                    }
                 }
                 Text("SCORE : \(viewModel.currentScore) ")
             }
@@ -54,18 +58,39 @@ struct CardView: View{
         }
     }
     
+    @State private var animatedBonusRemaining: Double = 0
+    private func startBonusTimeAnimation() {
+        animatedBonusRemaining = card.bonusRemaining
+        withAnimation(.linear(duration: card.bonusTimeRemaining)){
+            animatedBonusRemaining = 0
+        }
+    }
+    
     @ViewBuilder
     private func body(for size: CGSize) -> some View {
         if card.isFaceUp || !card.isMatched{
             
             ZStack{
-                Pie(startAngle: Angle.degrees(0 - 90), endAngle: Angle.degrees(110 - 90), clockwise: true)
-                    .padding(5).opacity(0.4)
+                Group{
+                    if card.isConsumingBonusTime {
+                        Pie(startAngle: Angle.degrees(0 - 90), endAngle: Angle.degrees(-animatedBonusRemaining*360 - 90), clockwise: true)
+                            .onAppear(){
+                                self.startBonusTimeAnimation()
+                        }
+                    } else {
+                        Pie(startAngle: Angle.degrees(0 - 90), endAngle: Angle.degrees(-card.bonusRemaining*360 - 90), clockwise: true)
+                    }
+                }
+                .padding(5).opacity(0.4)
+                
                 Text(self.card.content)
                     .font(Font.system(size: fontSize(for: size)))
+                    .rotationEffect(Angle.degrees(card.isMatched ? 360  : 0))
+                    .animation(card.isMatched ? Animation.linear(duration: 1).repeatForever(autoreverses: false) : .default)
             }
                 //.modifier(Cardify(isFaceUp: card.isFaceUp))
                 .cardify(isFaceUp: card.isFaceUp)
+                .transition(AnyTransition.scale)
         }
     }
     
--- MemoryGame/Pie.swift
+++ MemoryGame/Pie.swift
@@ -11,7 +11,17 @@ import SwiftUI
  struct Pie: Shape{
     var startAngle: Angle
     var endAngle: Angle
-    var clockwise: Bool = true
+    var clockwise: Bool = false
+    
+    var animatableData: AnimatablePair<Double, Double>{
+        get {
+            AnimatablePair(startAngle.radians, endAngle.radians)
+        }
+        set {
+            startAngle = Angle.radians(newValue.first)
+            endAngle = Angle.radians(newValue.second)
+        }
+    }
     
     func path(in rect: CGRect) -> Path {
         let center = CGPoint(x: rect.midX,y: rect.midY)
